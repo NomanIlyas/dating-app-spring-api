dd02eb944777d5dfad7249e1b1c461b006bfd98f
Noman Ilyas <noman.ilyas.software@gmail.com>
2021-12-16 12:09:19 +0500

    fix cors issue

--- src/main/java/com/example/myapp/controller/ProfileController.java
+++ src/main/java/com/example/myapp/controller/ProfileController.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(origins="*")
 @RequestMapping("/")
 @RestController
 public class ProfileController {
